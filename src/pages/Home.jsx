530fc2aea78ec6fd7d864f63de19c93a9026373d
Yuri Tokio <yuri@sheild.com.br>
2023-06-29 19:40:26 -0300

    Ajustes Css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import MovieCard from "../components/MovieCard";
 //useState - Hook para gerenciar o estado dos filmes
 //useEffect - Hook para fazer a chamada da API quando a pagina carregar
 
+import "../Styles/MoviesGrid.css"
+
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 // pegando chaves e valores do .env
@@ -28,7 +30,7 @@ const Home = () => {
 
     return (
         <div className="container">
-            <h2 className="title">Melhores filmes:</h2>
+            <h2 className="title">Melhores filmes</h2>
             <div className="movies-container">
                 {topMovies.length > 0 &&
                     topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
