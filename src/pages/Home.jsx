f62f66b75164ef4c6e517fe4bd97cde38b47cdcd
Yuri Tokio <yuri@sheild.com.br>
2023-06-29 19:15:01 -0300

    API funcionando e trazendo os melhores filmes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,40 @@
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+//useState - Hook para gerenciar o estado dos filmes
+//useEffect - Hook para fazer a chamada da API quando a pagina carregar
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+// pegando chaves e valores do .env
+
 const Home = () => {
-    return(
-        <div>
-            <h2>Home</h2>
+    const [topMovies, setTopMovies] = useState([]);
+
+    const getTopRatedMovies = async (url) => {
+
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setTopMovies(data.results);
+    };
+
+    useEffect(() => {
+
+        const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+
+        getTopRatedMovies(topRatedUrl);
+        //isso faz a chamada do fetch e podemos verificar em network preview
+    }, []);
+
+    return (
+        <div className="container">
+            <h2 className="title">Melhores filmes:</h2>
+            <div className="movies-container">
+                {topMovies.length > 0 &&
+                    topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Home;
