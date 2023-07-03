437990fe28536c2188542392701e5036ea6eb416
Yuri Tokio <yuri@sheild.com.br>
2023-07-03 19:25:54 -0300

    Dados vindos da api ok

--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,18 @@ import Home from './pages/Home'
 import Movie from './pages/Movie'
 import Search from './pages/Search'
 
+import "./Styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
-          <Route path='/' element={<Home />} />
-          <Route path='movie/id:' element={<Movie />} />
-          <Route path='search' element={<Search />} />
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="search" element={<Search />} />
         </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
