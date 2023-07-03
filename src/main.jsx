52d4e42d017ca16c69020429085e5ca417beca93
Yuri Tokio <yuri@sheild.com.br>
2023-07-03 17:51:19 -0300

    Pesquisa OK

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import App from './App'
 import Home from './pages/Home'
-import Movie from './pages/movie'
+import Movie from './pages/Movie'
 import Search from './pages/Search'
 
 
