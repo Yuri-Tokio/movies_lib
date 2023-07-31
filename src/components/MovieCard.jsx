7b2423a7e7301c0737864d312baa8ec50f6f8c39
Yuri Tokio <yuri@sheild.com.br>
2023-07-31 09:19:01 -0300

    Update average

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,12 +5,15 @@ import { FaStar } from "react-icons/fa";
 const imagesURL = import.meta.env.VITE_IMG;
 
 const MovieCard = ({ movie, showLink = true }) => {
+  
+  let average_Film = parseFloat(movie.vote_average);
+
   return (
     <div className="movie-card">
       <img src={imagesURL + movie.poster_path} alt={movie.title} />
       <h2>{movie.title}</h2>
       <p>
-        <FaStar /> {movie.vote_average}
+        <FaStar /> {average_Film.toFixed(1)}
       </p>
       {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
     </div>
